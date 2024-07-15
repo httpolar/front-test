2911b78d7846ada7610eebceda185d6cee4e3c58
httpolar <dev@firefly.party>
2024-07-15 12:48:35 +0400

    feat: inventory bottom element

---
+++ src/components/InventoryBottom.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import XIcon from "@/components/icons/XIcon.vue";
+</script>
+
+<template>
+  <div class="relative rounded-md border p-4 pr-16 dark:border-neutral-700 dark:bg-neutral-800">
+    <div class="h-9 animate-pulse rounded-md dark:bg-neutral-700"></div>
+    <div class="absolute right-0 top-0 mr-2 mt-2">
+      <XIcon class="cursor-pointer" />
+    </div>
+  </div>
+</template>
