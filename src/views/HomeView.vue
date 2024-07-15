60fab46fc74ff991a02ac330fa0be66b0064beca
httpolar <dev@firefly.party>
2024-07-15 17:42:54 +0400

    feat: display selected item with transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,21 +5,27 @@ import InventorySlot from "@/components/InventorySlot.vue";
 import MainWrapper from "@/components/MainWrapper.vue";
 import { cn } from "@/lib/cn";
 import { useInventoryStore } from "@/stores/inventory";
+import { vOnClickOutside } from "@vueuse/components";
+import InventorySelectedItem from "@/components/InventorySelectedItem.vue";
+import Button from "@/components/Button.vue";
 
-const inventoryStore = useInventoryStore();
+const inventory = useInventoryStore();
 </script>
 
 <template>
   <MainWrapper class="flex items-center justify-center">
     <div class="flex w-fit flex-col gap-6">
+      <div>
+        <Button variant="ghost" @click="inventory.reset">Сбросить состояние</Button>
+      </div>
       <div class="flex flex-row gap-6">
         <InventoryDescription />
 
         <div
-          class="grid auto-cols-[100px] grid-cols-1 gap-[1px] rounded-md border dark:border-neutral-700 dark:bg-neutral-700"
+          class="relative grid auto-cols-[100px] grid-cols-1 gap-[1px] rounded-md border dark:border-neutral-700 dark:bg-neutral-700"
         >
           <div
-            v-for="(chunk, rowIdx) in inventoryStore.items"
+            v-for="(chunk, rowIdx) in inventory.items"
             :key="`row-${rowIdx}`"
             class="grid auto-cols-[100px] grid-flow-col grid-rows-1 gap-[1px]"
           >
@@ -31,17 +37,36 @@ const inventoryStore = useInventoryStore();
                 cn({
                   'rounded-tl-md': rowIdx === 0 && colIdx === 0,
                   'rounded-tr-md': rowIdx === 0 && colIdx === chunk.length - 1,
-                  'rounded-bl-md': rowIdx === inventoryStore.items.length - 1 && colIdx === 0,
+                  'rounded-bl-md': rowIdx === inventory.items.length - 1 && colIdx === 0,
                   'rounded-br-md':
-                    rowIdx === inventoryStore.items.length - 1 && colIdx === chunk.length - 1
+                    rowIdx === inventory.items.length - 1 && colIdx === chunk.length - 1
                 })
               "
               :rowIdx="rowIdx"
               :colIdx="colIdx"
-              :data-row="rowIdx"
-              :data-col="colIdx"
             />
           </div>
+
+          <Transition
+            enterFromClass="opacity-0 translate-x-12"
+            enterToClass="opacity-100"
+            enterActiveClass="transition duration-300"
+            leaveFromClass="opacity-100"
+            leaveToClass="opacity-0 translate-x-12"
+            leaveActiveClass="transition duration-300"
+          >
+            <InventorySelectedItem
+              v-if="
+                inventory.selectedItem != null &&
+                inventory.selected[0] != null &&
+                inventory.selected[1] != null
+              "
+              :item="inventory.selectedItem"
+              :rowIdx="inventory.selected[0]"
+              :colIdx="inventory.selected[1]"
+              v-on-click-outside="inventory.deselect"
+            ></InventorySelectedItem>
+          </Transition>
         </div>
       </div>
 
